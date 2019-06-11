202eb80e4b15a11cfdacf47b20f47bd97d5ce549
ravenoak <ravenoak@virtualxistenz.com>
2019-06-11 07:49:40 -0700

    Checkpoint

---
+++ integration_tests/integration_runner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+
+from willamette_client.client import WillametteClient
+
+
+def load_test_data():
+    pass
+
+
+def main():
+    url = os.getenv('WILLAMETTE_API_URL', 'http://localhost:5000/api')
+    client = WillametteClient(url)
+    load_test_data()
+
+
+if __name__ == 'main':
+    main()
